28e466b617c9bf3c94e8fcad100a6158a3e11e3c
vamsiraju835@gmail.com <vamsiraju835@gmail.com>
2023-05-23 16:28:32 -0400

    File Changes

--- Frontend-ReactJS/react-library/src/App.tsx
+++ Frontend-ReactJS/react-library/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/HomePage/ExplorerTopBooks';
+import { ExploreTopBooks } from './layouts/HomePage/components/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/components/Carousel';
+import { Heros } from './layouts/HomePage/components/Heros';
+import { LibraryServices } from './layouts/HomePage/components/LibraryServices';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
 
 
-function App() {
+export const App = () => {
   return (
     <div>
-      <Navbar />
-      <ExploreTopBooks />
+      <HomePage/>
     </div>
   );
 }
-
-export default App;
