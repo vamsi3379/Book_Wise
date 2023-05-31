317d6c3a53ee657500eae5792cec9138919a24b4
vamsiraju835@gmail.com <vamsiraju835@gmail.com>
2023-05-30 21:34:37 -0400

    Added Backend folder and code modification

--- Frontend/src/layouts/HomePage/HomePage.tsx
+++ Frontend/src/layouts/HomePage/HomePage.tsx
@@ -2,15 +2,19 @@ import { ExploreTopBooks } from './components/ExploreTopBooks';
 import { Carousel } from './components/Carousel';
 import { Heros } from './components/Heros';
 import { LibraryServices } from './components/LibraryServices';
+import { Navbar } from '../NavbarAndFooter/Navbar';
+import { Footer } from '../NavbarAndFooter/Footer';
 
 
 export const HomePage = () => {
     return (
       <>
+      <Navbar />
         <ExploreTopBooks />
         <Carousel/>
         <Heros/>
         <LibraryServices/>
+      <Footer />
       </>
     )
 }
