3990c4867f80b40ff609dd0f9b683e8acab1991a
vamsiraju835@gmail.com <vamsiraju835@gmail.com>
2023-06-20 21:53:38 -0400

    Production File for EC2

--- Frontend/src/lib/oktaConfig.ts
+++ Frontend/src/lib/oktaConfig.ts
@@ -1,7 +1,8 @@
 export const oktaConfig = {
     clientId: `0oa9httyvaMdgxxzl5d7`,
     issuer: `https://dev-78760789.okta.com/oauth2/default`,
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: `${process.env.OKTA_LOGIN_URL_CALLBACK}`,
+    // redirectUri: 'https://ec2-18-189-18-248.us-east-2.compute.amazonaws.com/login/callback',
     scopes: ['openid','profile','email'],
     pkce: true,
     disableHttpCheck: true,
